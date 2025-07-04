a3c6078e96c4e03f85965b9ad149bd684ff183a7
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-07-04 20:46:31 +0530

    Fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,10 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-// export const authClient = createAuthClient({
-//   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-// }); #update
-
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://stream-nest-seven.vercel.app",
-});
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+}); 
+
 
