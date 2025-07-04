a04f55d6342c4e653fa8eecedf1b9781b7d62232
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-07-04 21:12:34 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,54 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: process.env.NODE_ENV !== "production",
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: process.env.NODE_ENV !== "production",
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "stream-nest.b-cdn.net",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+//   experimental: {
+//     serverActions: {
+//       bodySizeLimit: "50mb",
+//     },
+//   },
+//   output: "standalone",
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: process.env.NODE_ENV !== "production",
+  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: process.env.NODE_ENV !== "production",
+    ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "stream-nest.b-cdn.net",
-        pathname: "/**",
-      },
+      { hostname: "stream-nest.b-cdn.net", protocol: "https", pathname: "/**" },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
@@ -32,7 +66,6 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "50mb",
     },
   },
-  output: "standalone",
 };
 
 export default nextConfig;
